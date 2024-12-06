732af3de04a5f375ff61cbc02e98d8f9e338fc0b
Ahmad Khidir <khidirahmad05@gmail.com>
2024-12-06 01:47:06 +0100

    resolved mongodb connection

---
+++ types/global.d.ts
@@ -0,0 +1,15 @@
+import { Mongoose } from 'mongoose';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongoose: {
+        conn: Mongoose | null;
+        promise: Promise<Mongoose> | null;
+      };
+    }
+  }
+}
+
+
+export {}
