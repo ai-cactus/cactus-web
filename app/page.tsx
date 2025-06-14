3103409c1879eb1d9389939e3165b8de966cc596
caprule <folabas2@gmail.com>
2025-06-14 13:31:00 +0000

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,6 @@ import { AppRoutes } from '@/utils/routes';
 
 export default function Home() {
   redirect(AppRoutes.main.dashboard.path);
+  // redirect(AppRoutes.auth.login.path);
   return null;
 }
